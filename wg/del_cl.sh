051c7403900d7a93d941ffce674d37bbe5a57dbe
Djonyx756 <75791040+Djonyx756@users.noreply.github.com>
2023-11-29 21:20:16 +0300

    Update del_cl.sh

--- wg/del_cl.sh
+++ wg/del_cl.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-# Запрос адреса от пользователя
-read -p "Введите адрес (например, 10.10.0.8): " address
+#!bin/bash
 
-# Поиск и удаление строки с указанным адресом
-sed -i "/AllowedIPs = $address\/32/{N;N;d;}" /etc/wireguard/wg0.conf
-
-echo "Строка с адресом $address и две предыдущие строки были удалены из файла."
-
-systemctl restart wg-quick@wg0
+ip="6"
+my_variable=$(grep -n "10.10.0.${ip}" /etc/wireguard/wg0.conf | cut -d ':' -f 1)
+sed -i "$(($my_variable-2)),$my_variable d" /etc/wireguard/wg0.conf
