1396bc736f8be7c800e89522091b6b17eee3cda7
Djonyx756 <75791040+Djonyx756@users.noreply.github.com>
2023-12-02 17:52:49 +0300

    Update git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Сохраняем текущее значение api_tg
+current_mainid=$(grep "mainid" wg_telegram/config.py | sed 's/.*\[\(.*\)\].*/\1/')
 current_api_tg=$(grep "api_tg =" wg_telegram/config.py | sed 's/.*"\(.*\)".*/\1/')
 
 # Заменяем значение api_tg на пустую строку
+sed -i 's/mainid = \[.*\]/mainid = \[\]/' wg_telegram/config.py
 sed -i 's/api_tg = ".*"/api_tg = ""/' wg_telegram/config.py
 
 # Переходим в папку с проектом Git
@@ -18,4 +20,5 @@ git commit -m "update main.py"
 git push origin main
 
 # Восстанавливаем предыдущее значение api_tg
+sed -i "s/mainid = \[\]/mainid = \[$current_mainid\]/" config.py
 sed -i "s/api_tg = \"\"/api_tg = \"$current_api_tg\"/" config.py
