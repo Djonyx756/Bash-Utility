8b44b90104ce1b9e361ed052674ad62db5f54f47
Djonyx756 <75791040+Djonyx756@users.noreply.github.com>
2023-12-02 16:09:55 +0300

    Create git_push.sh

---
+++ git_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Сохраняем текущее значение api_tg
+current_api_tg=$(grep "api_tg =" wg_telegram/config.py | sed 's/.*"\(.*\)".*/\1/')
+
+# Заменяем значение api_tg на пустую строку
+sed -i 's/api_tg = ".*"/api_tg = ""/' wg_telegram/config.py
+
+# Переходим в папку с проектом Git
+cd wg_telegram
+
+# Выполняем git add и другие команды
+git add .
+git rm --cached cofigs.txt
+git rm --cached variables.sh
+git rm --cached -r __pycache__/
+git commit -m "update main.py"
+git push origin main
+
+# Восстанавливаем предыдущее значение api_tg
+sed -i "s/api_tg = \"\"/api_tg = \"$current_api_tg\"/" config.py
